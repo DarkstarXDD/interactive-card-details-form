e671282acca6517edb58351befc51f2888ee0c79
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2025-04-11 09:37:56 +0530

    fix: datefield granularity issue

--- src/components/ui/RACDateField.tsx
+++ src/components/ui/RACDateField.tsx
@@ -18,20 +18,21 @@ export default function RACDateField<T extends DateValue>({
   ...props
 }: RACDateField<T>) {
   return (
-    <DateField
-      {...props}
-      className={cn("grid gap-2", className)}
-      granularity="month"
-    >
+    <DateField {...props} className={cn("grid gap-2", className)}>
       <RACLabel>{label}</RACLabel>
       <DateInput className="flex gap-2">
         {(segment) => (
-          <DateSegment
-            className="border-border ring-ring text-foreground aria-[invalid]:border-error aria-[invalid]:ring-error data-[placeholder]:text-primary-foreground-muted w-20 min-w-0 rounded-lg border px-4 py-2 text-base outline-none focus-visible:ring-2 data-[placeholder]:uppercase data-[type='literal']:hidden"
-            segment={segment}
-          />
+          <>
+            {segment.type !== "day" && (
+              <DateSegment
+                className="border-border ring-ring text-foreground aria-[invalid]:border-error aria-[invalid]:ring-error data-[placeholder]:text-primary-foreground-muted w-20 min-w-0 rounded-lg border px-4 py-2 text-base outline-none focus-visible:ring-2 data-[placeholder]:uppercase data-[type='literal']:hidden"
+                segment={segment}
+              />
+            )}
+          </>
         )}
       </DateInput>
+
       <RACFieldError>{errorMessage}</RACFieldError>
     </DateField>
   )
