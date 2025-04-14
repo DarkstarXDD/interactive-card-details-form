a121cb0e05d3fa062072159eb5c441cc0a2a6554
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2025-04-14 19:20:45 +0530

    refactor: format values to display

--- src/components/CardBack.tsx
+++ src/components/CardBack.tsx
@@ -1,11 +1,13 @@
-export default function CardBack() {
+export default function CardBack({ cvc }: { cvc: string | undefined }) {
   return (
     <div className="col-start-2 col-end-7 row-start-1 row-end-8 grid lg:row-start-8 lg:row-end-14">
       <div className="bg-card-bg flex h-full flex-col items-center gap-4 rounded-md py-3 lg:gap-6 lg:rounded-lg lg:py-5">
         <div className="bg-card-black h-7 w-full lg:h-14"></div>
         <div className="flex w-full px-6 lg:px-10">
           <div className="bg-card-grey flex h-7 w-full items-center justify-end rounded px-3 lg:h-10 lg:px-4">
-            <span className="text-foreground text-sm font-medium">000</span>
+            <span className="text-foreground text-sm font-medium">
+              {cvc ?? 123}
+            </span>
           </div>
         </div>
       </div>
--- src/components/CardFront.tsx
+++ src/components/CardFront.tsx
@@ -1,4 +1,14 @@
-export default function CardFront() {
+export default function CardFront({
+  cardholderName,
+  cardNumber,
+  expMonth,
+  expYear,
+}: {
+  cardholderName: string | undefined
+  cardNumber: string | undefined
+  expMonth: number | undefined
+  expYear: number | undefined
+}) {
   return (
     <div className="col-start-1 col-end-6 row-start-5 row-end-12 grid grid-rows-1 lg:row-start-1 lg:row-end-7">
       <div className="grid h-full gap-9 rounded-md bg-[url(../assets/bg-card-front.png)] bg-cover bg-center bg-no-repeat p-5 lg:gap-16 lg:rounded-lg lg:p-8">
@@ -9,14 +19,29 @@ export default function CardFront() {
 
         <div className="grid gap-4 lg:gap-6">
           <p className="xs:text-base truncate text-xs font-medium tracking-[2px] text-nowrap text-white lg:text-xl">
-            0000 0000 0000 0000
+            {cardNumber ? formatCardNumber(cardNumber) : "0000 0000 0000 0000"}
           </p>
-          <div className="flex justify-between text-xs font-medium tracking-[1px] text-white lg:text-base">
-            <p>JANE APPLESEED</p>
-            <p>00/00</p>
+          <div className="flex justify-between text-xs font-medium tracking-[1px] text-white uppercase lg:text-base">
+            <p>{cardholderName ?? "Jane Appleseed"}</p>
+            <p>
+              {expMonth ? formatMonth(expMonth) : "00"}/
+              {expYear ? fomrmatYear(expYear) : "0000"}
+            </p>
           </div>
         </div>
       </div>
     </div>
   )
+}
+
+function formatCardNumber(cardNumber: string) {
+  return cardNumber.replace(/(.{4})/g, "$1 ").trim()
+}
+
+function formatMonth(month: number) {
+  return String(month).padStart(2, "0")
+}
+
+function fomrmatYear(year: number) {
+  return String(year).padStart(4, "0")
 }
