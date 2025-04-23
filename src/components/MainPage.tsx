651a70b5b134da9fce00ab271327a2373d27642d
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2025-04-23 10:31:27 +0530

    style: remove unwanted styles

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -24,10 +24,10 @@ export default function MainPage() {
   }
 
   return (
-    <div className="grid w-full lg:grid-cols-[10rem_20rem_1fr] xl:grid-cols-[10rem_20rem_13.75rem_1fr]">
-      <div className="col-start-1 row-start-1 row-end-2 h-60 bg-[url(../assets/bg-main-mobile.png)] bg-cover bg-no-repeat lg:col-start-1 lg:col-end-3 lg:row-start-1 lg:min-h-dvh lg:bg-[url(../assets/bg-main-desktop.png)]"></div>
+    <div className="grid w-full lg:grid-cols-[30rem_1fr] xl:grid-cols-[10rem_20rem_13.75rem_1fr]">
+      <div className="col-start-1 row-start-1 row-end-2 h-60 bg-[url(../assets/bg-main-mobile.png)] bg-cover bg-no-repeat lg:col-end-2 lg:min-h-dvh lg:bg-[url(../assets/bg-main-desktop.png)] xl:col-end-3"></div>
 
-      <div className="col-start-1 row-start-1 row-end-3 m-auto grid w-full max-w-sm grid-cols-6 grid-rows-[repeat(11,_minmax(0,_1fr))] px-4 pt-8 lg:col-start-1 lg:col-end-3 lg:row-start-1 lg:max-w-md lg:grid-rows-[repeat(13,_minmax(0,_1fr))] lg:p-0 xl:col-start-2 xl:col-end-4 xl:max-w-135">
+      <div className="col-start-1 row-start-1 row-end-3 m-auto grid w-full max-w-sm grid-cols-6 grid-rows-[repeat(11,_minmax(0,_1fr))] px-4 pt-8 lg:col-end-2 lg:max-w-md lg:grid-rows-[repeat(13,_minmax(0,_1fr))] lg:p-0 xl:col-start-2 xl:col-end-4 xl:max-w-135">
         <CardBack cvc={formValues?.cvc} />
         <CardFront
           cardholderName={formValues?.cardholderName}
